5d2f7a31498d1ac02e3bc7bdd51298c664d4787b
m7lewandowska <marfalewandowska@gmail.com>
2020-01-26 12:07:59 +0100

    added methods

--- app/src/main/java/com/example/androidpatternlockv2/MainActivity.java
+++ app/src/main/java/com/example/androidpatternlockv2/MainActivity.java
@@ -4,11 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements PatternLockViewListener {
+
+    PatternLockView patternLockView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        patternLockView = (PatternLockView)findViewById(R.id.pattern_lock_view);
+    }
+
+    @Override
+    public void onStarted() {
+        
+    }
+
+    @Override
+    public void onProgress(List<PatternLockView.Dot> progressPattern) {
+
+    }
+
+    @Override
+    public void onComplete(List<PatternLockView.Dot> pattern) {
+
+    }
+
+    @Override
+    public void onCleared() {
+
     }
 }
